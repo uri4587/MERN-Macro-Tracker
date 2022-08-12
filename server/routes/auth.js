71db83f60af8bfff287a8516e3218357fdb20f6a
Uri <urinemirovsky@yahoo.com>
2022-08-11 21:33:38 -0400

    created login route, implemented json web token, and get route for current user email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 //POST Route: '/signup'
 //Description: Signs user up for account
@@ -37,6 +39,74 @@ router.post('/signup', async (req, res) => {
         })
         
     }
+})
+
+// POST Route: '/signin'
+// Signs in user
+router.post('/signin', (req, res) => {
+    const userLoggingIn = req.body;
+
+    if(!userLoggingIn.email || !userLoggingIn.password) {
+        return res.status(422).json({error: 'Please provide email and password'})
+    };
+
+    User.findOne({email: userLoggingIn.email})
+    .then(dbUser => {
+        if(!dbUser) {
+            return res.json({
+                message: "Invalid Email or Password"
+            })
+        }
+        bcrypt.compare(userLoggingIn.password, dbUser.password)
+        .then(isCorrect => {
+            if(isCorrect) {
+                const payload = {
+                    id: dbUser._id,
+                    email: dbUser.email,
+                }
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    {expiresIn: 86400},
+                    (err, token) => {
+                        if(err) return res.json({message: err})
+                        return res.json({
+                            message: "Success",
+                            token: "Bearer" + token
+                        })
+                    }
+                )
+            } else {
+                return res.json({
+                    message: "Invalid Email or Password"
+                })
+            }
+        })
+    })
+})
+
+function verifyJWT(req, res, next) {
+    const token = req.headers["x-access-token"]?.split('')[1]
+
+    if(token) {
+        jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+            if(err) return res.json({
+                isLoggedIn: false, 
+                message: "Failed To Authenticate"
+            })
+            req.user = {};
+            req.user.id = decoded.id;
+            req.user.email = decoded.email;
+            next()
+        })
+    } else {
+        res.json({message: "Incorrect Token Given", isLoggedIn: false})
+    }
+}
+
+//GET logged In User 
+router.get('/getUsername', verifyJWT, (req, res) => {
+    res.json({isLoggedIn: true, email: req.user.email})
 })
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
-router.get('/', function(req, res) {
-    res.send('Users Index Page');
-});
+// router.get('/users', async function(req, res) {
+//     try {
+//         const data = await 
+//     } catch (err) {
+//         res.status(500).json({message: err.message});
+//     }
+// });
 
-router.get('/list', function(req, res) {
-    res.send('Users List Page');
-});
+
 
 module.exports = router;
